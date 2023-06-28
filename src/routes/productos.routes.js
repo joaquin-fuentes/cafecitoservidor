27bb88d19ab477ac843f59f71f095d30c0a335dd
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-06-28 11:35:43 -0300

    agregamos una validacion con express-validator

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
-import { borrarProducto, crearProducto, editarProducto, obtenerProducto, obtenerProductos } from "../controllers/productos.controllers";
+import {
+    borrarProducto,
+    crearProducto,
+    editarProducto,
+    obtenerProducto,
+    obtenerProductos
+} from "../controllers/productos.controllers";
+import {check} from "express-validator"
 
 const router = Router()
 
 
 router.route("/productos")
     .get(obtenerProductos)
-    .post(crearProducto)
+    .post([check("nombreProducto")
+           .notEmpty()
+           .withMessage("El nombre del producto es un dato obligatorio")]
+                ,crearProducto)
 router.route("/productos/:id")
     .get(obtenerProducto)
     .delete(borrarProducto)
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,9 +3,6 @@ import { crearUsuario, obtenerUsuarios } from "../controllers/usuarios.controlle
 
 const router = Router()
 
-// app.get("/prueba", (req, res)=>{
-//     res.send("esto es una prueba de la peticion GET a mi backend")
-// })
 
 router.route("/usuarios")
 .get(obtenerUsuarios)
